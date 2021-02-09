d4a3e66a2584e5832bd2b847efe5b58f432275ab
Mohammed Thawfeeq Ishaq <benzmclaren@gmail.com>
2021-02-09 12:50:13 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title("Turtle Crossing")
+screen.tracer(0)
+
+player = Player()
+cars = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.up, "Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    cars.create_car()
+    cars.move_cars()
+
+    # Detecting Collision with the Car
+    for car in cars.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.ycor() > 280:
+        player.finish_line()
+        cars.level_up()
+        scoreboard.point()
+
+screen.exitonclick()
